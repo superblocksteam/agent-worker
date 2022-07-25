d20c7b24c2d146ad59ff48664671cbd15c41ca03
superblocksadmin <admin@superblockshq.com>
2022-07-25 17:03:47 +0000

    Releasing worker version 0.1849.0

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -7,6 +7,7 @@ import sb_dynamodb_0_0_4 from 'sb-dynamodb-0.0.4';
 import sb_email_0_0_4 from 'sb-email-0.0.4';
 import sb_graphql_0_0_5 from 'sb-graphql-0.0.5';
 import sb_gsheets_0_0_10 from 'sb-gsheets-0.0.10';
+import sb_gsheets_0_0_11 from 'sb-gsheets-0.0.11';
 import sb_javascript_0_0_6 from 'sb-javascript-0.0.6';
 import sb_javascript_0_0_7 from 'sb-javascript-0.0.7';
 import sb_mariadb_0_0_2 from 'sb-mariadb-0.0.2';
@@ -15,6 +16,7 @@ import sb_mssql_0_0_2 from 'sb-mssql-0.0.2';
 import sb_mysql_0_0_2 from 'sb-mysql-0.0.2';
 import sb_postgres_0_0_2 from 'sb-postgres-0.0.2';
 import sb_python_0_0_5 from 'sb-python-0.0.5';
+import sb_python_0_0_6 from 'sb-python-0.0.6';
 import sb_redshift_0_0_2 from 'sb-redshift-0.0.2';
 import sb_restapi_0_0_9 from 'sb-restapi-0.0.9';
 import sb_restapiintegration_0_0_9 from 'sb-restapiintegration-0.0.9';
@@ -31,6 +33,7 @@ const dependencies: Record<string, BasePlugin | unknown> = {
   'sb-email-0.0.4': new sb_email_0_0_4(),
   'sb-graphql-0.0.5': new sb_graphql_0_0_5(),
   'sb-gsheets-0.0.10': new sb_gsheets_0_0_10(),
+  'sb-gsheets-0.0.11': new sb_gsheets_0_0_11(),
   'sb-javascript-0.0.6': new sb_javascript_0_0_6(),
   'sb-javascript-0.0.7': new sb_javascript_0_0_7(),
   'sb-mariadb-0.0.2': new sb_mariadb_0_0_2(),
@@ -39,6 +42,7 @@ const dependencies: Record<string, BasePlugin | unknown> = {
   'sb-mysql-0.0.2': new sb_mysql_0_0_2(),
   'sb-postgres-0.0.2': new sb_postgres_0_0_2(),
   'sb-python-0.0.5': new sb_python_0_0_5(),
+  'sb-python-0.0.6': new sb_python_0_0_6(),
   'sb-redshift-0.0.2': new sb_redshift_0_0_2(),
   'sb-restapi-0.0.9': new sb_restapi_0_0_9(),
   'sb-restapiintegration-0.0.9': new sb_restapiintegration_0_0_9(),
