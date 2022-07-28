e94156932b39250d98b1d8c6c65084a11aeef073
superblocksadmin <admin@superblockshq.com>
2022-07-28 19:29:21 +0000

    Releasing worker version 0.1875.0

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -2,54 +2,82 @@
 
 import { BasePlugin } from '@superblocksteam/shared-backend';
 import sb_bigquery_0_0_2 from 'sb-bigquery-0.0.2';
+import sb_bigquery_0_0_4 from 'sb-bigquery-0.0.4';
 import sb_dynamodb_0_0_3 from 'sb-dynamodb-0.0.3';
-import sb_dynamodb_0_0_4 from 'sb-dynamodb-0.0.4';
+import sb_dynamodb_0_0_6 from 'sb-dynamodb-0.0.6';
 import sb_email_0_0_4 from 'sb-email-0.0.4';
+import sb_email_0_0_6 from 'sb-email-0.0.6';
 import sb_graphql_0_0_5 from 'sb-graphql-0.0.5';
+import sb_graphql_0_0_7 from 'sb-graphql-0.0.7';
 import sb_gsheets_0_0_10 from 'sb-gsheets-0.0.10';
-import sb_gsheets_0_0_11 from 'sb-gsheets-0.0.11';
+import sb_gsheets_0_0_13 from 'sb-gsheets-0.0.13';
 import sb_javascript_0_0_6 from 'sb-javascript-0.0.6';
 import sb_javascript_0_0_7 from 'sb-javascript-0.0.7';
+import sb_javascript_0_0_8 from 'sb-javascript-0.0.8';
 import sb_mariadb_0_0_2 from 'sb-mariadb-0.0.2';
+import sb_mariadb_0_0_4 from 'sb-mariadb-0.0.4';
 import sb_mongodb_0_0_3 from 'sb-mongodb-0.0.3';
+import sb_mongodb_0_0_5 from 'sb-mongodb-0.0.5';
 import sb_mssql_0_0_2 from 'sb-mssql-0.0.2';
+import sb_mssql_0_0_4 from 'sb-mssql-0.0.4';
 import sb_mysql_0_0_2 from 'sb-mysql-0.0.2';
+import sb_mysql_0_0_4 from 'sb-mysql-0.0.4';
 import sb_postgres_0_0_2 from 'sb-postgres-0.0.2';
+import sb_postgres_0_0_4 from 'sb-postgres-0.0.4';
 import sb_python_0_0_5 from 'sb-python-0.0.5';
 import sb_python_0_0_6 from 'sb-python-0.0.6';
 import sb_redshift_0_0_2 from 'sb-redshift-0.0.2';
+import sb_redshift_0_0_4 from 'sb-redshift-0.0.4';
+import sb_restapi_0_0_11 from 'sb-restapi-0.0.11';
 import sb_restapi_0_0_9 from 'sb-restapi-0.0.9';
+import sb_restapiintegration_0_0_11 from 'sb-restapiintegration-0.0.11';
 import sb_restapiintegration_0_0_9 from 'sb-restapiintegration-0.0.9';
 import sb_rockset_0_0_2 from 'sb-rockset-0.0.2';
+import sb_rockset_0_0_4 from 'sb-rockset-0.0.4';
 import sb_s3_0_0_4 from 'sb-s3-0.0.4';
-import sb_s3_0_0_5 from 'sb-s3-0.0.5';
+import sb_s3_0_0_7 from 'sb-s3-0.0.7';
 import sb_snowflake_0_0_2 from 'sb-snowflake-0.0.2';
+import sb_snowflake_0_0_4 from 'sb-snowflake-0.0.4';
 import sb_workflow_0_0_2 from 'sb-workflow-0.0.2';
 
 const dependencies: Record<string, BasePlugin | unknown> = {
   'sb-bigquery-0.0.2': new sb_bigquery_0_0_2(),
+  'sb-bigquery-0.0.4': new sb_bigquery_0_0_4(),
   'sb-dynamodb-0.0.3': new sb_dynamodb_0_0_3(),
-  'sb-dynamodb-0.0.4': new sb_dynamodb_0_0_4(),
+  'sb-dynamodb-0.0.6': new sb_dynamodb_0_0_6(),
   'sb-email-0.0.4': new sb_email_0_0_4(),
+  'sb-email-0.0.6': new sb_email_0_0_6(),
   'sb-graphql-0.0.5': new sb_graphql_0_0_5(),
+  'sb-graphql-0.0.7': new sb_graphql_0_0_7(),
   'sb-gsheets-0.0.10': new sb_gsheets_0_0_10(),
-  'sb-gsheets-0.0.11': new sb_gsheets_0_0_11(),
+  'sb-gsheets-0.0.13': new sb_gsheets_0_0_13(),
   'sb-javascript-0.0.6': new sb_javascript_0_0_6(),
   'sb-javascript-0.0.7': new sb_javascript_0_0_7(),
+  'sb-javascript-0.0.8': new sb_javascript_0_0_8(),
   'sb-mariadb-0.0.2': new sb_mariadb_0_0_2(),
+  'sb-mariadb-0.0.4': new sb_mariadb_0_0_4(),
   'sb-mongodb-0.0.3': new sb_mongodb_0_0_3(),
+  'sb-mongodb-0.0.5': new sb_mongodb_0_0_5(),
   'sb-mssql-0.0.2': new sb_mssql_0_0_2(),
+  'sb-mssql-0.0.4': new sb_mssql_0_0_4(),
   'sb-mysql-0.0.2': new sb_mysql_0_0_2(),
+  'sb-mysql-0.0.4': new sb_mysql_0_0_4(),
   'sb-postgres-0.0.2': new sb_postgres_0_0_2(),
+  'sb-postgres-0.0.4': new sb_postgres_0_0_4(),
   'sb-python-0.0.5': new sb_python_0_0_5(),
   'sb-python-0.0.6': new sb_python_0_0_6(),
   'sb-redshift-0.0.2': new sb_redshift_0_0_2(),
+  'sb-redshift-0.0.4': new sb_redshift_0_0_4(),
+  'sb-restapi-0.0.11': new sb_restapi_0_0_11(),
   'sb-restapi-0.0.9': new sb_restapi_0_0_9(),
+  'sb-restapiintegration-0.0.11': new sb_restapiintegration_0_0_11(),
   'sb-restapiintegration-0.0.9': new sb_restapiintegration_0_0_9(),
   'sb-rockset-0.0.2': new sb_rockset_0_0_2(),
+  'sb-rockset-0.0.4': new sb_rockset_0_0_4(),
   'sb-s3-0.0.4': new sb_s3_0_0_4(),
-  'sb-s3-0.0.5': new sb_s3_0_0_5(),
+  'sb-s3-0.0.7': new sb_s3_0_0_7(),
   'sb-snowflake-0.0.2': new sb_snowflake_0_0_2(),
+  'sb-snowflake-0.0.4': new sb_snowflake_0_0_4(),
   'sb-workflow-0.0.2': new sb_workflow_0_0_2()
 };
 
